46342b435618bf1e94198caa3aca78572bd7f2bd
arnaud-eb <arnaud.depierreux@easebest.com>
2023-12-22 09:27:36 +0100

    feat(page): enable smooth scrolling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 
+import SmoothScrolling from "@/components/SmoothScrolling";
+
 // import { Inter } from "next/font/google";
 
 // const inter = Inter({ subsets: ["latin"] });
@@ -17,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       {/* <body className="flex items-center justify-center h-screen w-screen m-0 text-black bg-white"> */}
-      <body>{children}</body>
+      <body>
+        <SmoothScrolling>{children}</SmoothScrolling>
+      </body>
       {/* <body className="bg-yellow-200 bg-gradient-radial from-yellow-600 from-10% via-transparent via-11% bg-[length:20px_20px] bg-repeat overflow-hidden">{children}</body> */}
     </html>
   );
